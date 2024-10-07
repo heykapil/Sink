fcf55b2b09ef850d47f51c900c0f71128310ad48
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2024-10-08 00:35:59 +0530

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'Links shortner',
+  description: 'Analytics, Link shortner.',
+  image: 'https://cdn.kapil.app/',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
